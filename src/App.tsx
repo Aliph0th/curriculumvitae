f41611feaba1d84ac11f184d9da4f373c8cc35f7
Aliph0th <theblacklord05@gmail.com>
2024-12-18 22:29:41 +0300

    feat: add tooltip component and svg support

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import Header from './components/Header';
+import About from './components/sections/About';
+import DownloadSvg from './assets/download.svg?react';
+import PdfSvg from './assets/pdf.svg?react';
+import Tooltiped from './components/Tooltiped';
+
 function App() {
-   return <div></div>;
+   return (
+      <div className="max-w-[1200px] mx-auto py-7 px-4">
+         <Header />
+         <main className="grid grid-cols-2 grid-rows-2 my-7">
+            <About className="row-start-1 col-start-1 col-span-2" />
+            <div className="row-start-1 col-start-1 col-span-2 justify-self-end self-center">
+               <Tooltiped
+                  tooltip={
+                     <span className="flex">
+                        Get&nbsp;
+                        <PdfSvg className="w-5" />
+                        pdf
+                     </span>
+                  }
+               >
+                  <DownloadSvg className="w-8 hover:fill-accent" />
+               </Tooltiped>
+            </div>
+            <aside className="row-start-2 col-start-1">2</aside>
+            <div className="row-start-2 col-start-2">ss</div>
+         </main>
+      </div>
+   );
 }
 
 export default App;
---
+++ src/components/Tooltiped.tsx
@@ -0,0 +1,29 @@
+import { FC, ReactNode, useState } from 'react';
+
+interface TooltipedProps {
+   children: ReactNode;
+   tooltip: ReactNode;
+}
+
+const Tooltiped: FC<TooltipedProps> = ({ children, tooltip }) => {
+   const [isVisible, setIsVisible] = useState(true);
+
+   return (
+      <div className="relative inline-block">
+         <div
+            onMouseEnter={() => setIsVisible(true)}
+            onMouseLeave={() => setIsVisible(false)}
+            className="cursor-pointer"
+         >
+            {children}
+         </div>
+         {isVisible && (
+            <div className="absolute bottom-full mb-2 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white text-sm rounded py-1 px-2">
+               {tooltip}
+            </div>
+         )}
+      </div>
+   );
+};
+
+export default Tooltiped;
